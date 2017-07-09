8483892d5bcd43381d28ea3761a016e8f9da8833
CQuinz <christian.quinlan36@gmail.com>
2017-07-09 15:22:47 +0100

    added table to view articles

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,13 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!--THIS IS THE HEADER-->
+<?php include "../includes/db.php" ?>
+
 	<meta charset="UTF-8">
 	<title>Admin Page</title>
+	
 	<!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     
@@ -67,7 +72,7 @@
 	 
 	 <section id="admin_area">
 		 <div id="admin_content">
-		 	<table class="table table-striped">
+		 	<table class="table table-striped table-hover">
 		 		<thead class="thead-inverse">
 		 			<th>Article ID</th>
 		 			<th>Article Title</th>
@@ -76,18 +81,40 @@
 		 			<th>Article Body</th>
 		 			<th>Edit</th>
 		 			<th>Delete</th>
-		 			
-		 			
 		 		</thead>
+		 		
+		 		<?php
+				//CREATE QUERY
+				$query = "SELECT * FROM articles";
+				
+				//CONNECT QUERY TO DB
+				$show_all_articles= mysqli_query($db,$query);
+				
+				//Loop through and display results
+				while($row =mysqli_fetch_assoc($show_all_articles)){
+			
+					//Creating variables for the array
+					$article_id = $row['article_id'];
+					$article_title = $row['article_title'];
+					$article_author = $row['article_author'];
+					$article_date = $row['article_date'];
+					$article_body_abbv = substr($row['article_body'],0, 20);
+			
+		?>
+				
+		 		
 		 		<tbody>
-		 			<td>123</td>
-		 			<td>Yoda</td>
-		 			<td>Micky Mouse</td>
-		 			<td>12/10/17</td>
-		 			<td>rhehetjjrjrmryrs</td>
+		 			<td><?php echo "{$article_id}"; ?></td>
+		 			<td><?php echo "{$article_title}"; ?></td>
+		 			<td><?php echo "{$article_date}"; ?></td>
+		 			<td><?php echo "{$article_author}"; ?></td>
+		 			<td><?php echo "{$article_body_abbv}"; ?></td>
 		 			<td><a href="#">Edit</a></td>
 		 			<td><a href="#">Delete</a></td>
+		 			
+		 			<?php } //Loop through the results ?>
 		 		</tbody>
+		 		
 		 	</table>
 
 		 </div>
