3e2dac69f9ce64dcbb60af60f5f2ed3e20c529b9
CQuinz <christian.quinlan36@gmail.com>
2017-07-12 19:49:20 +0100

    style adjustments to admin page, added view all articles to includes folder

---
+++ admin/admin_includes/view_all_articles.php
@@ -0,0 +1,52 @@
+			<!--VIEW ALL ARTICLES-->
+			
+			<h2 class="text-center display-4 ">View All Posts</h2>
+			
+	 		<table class="table table-striped table-hover">
+		 		<thead class="thead-inverse">
+		 			<tr>
+						<th>Article ID</th>
+						<th>Article Title</th>
+						<th>Article Author</th>
+						<th>Article Date</th>
+						<th>Article Body</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					</tr>
+		 		</thead>
+		 		
+		 		<?php
+				//CREATE QUERY SHOW ALL ARTICLES
+				$query = "SELECT * FROM articles";
+				
+				//CONNECT QUERY TO DB
+				$show_all_articles= mysqli_query($db,$query);
+				
+				//Loop through and display results
+				while($row =mysqli_fetch_assoc($show_all_articles)){
+			
+					//Creating variables for the array
+					$article_id = $row['article_id'];
+					$article_title = $row['article_title'];
+					$article_author = $row['article_author'];
+					$article_date = $row['article_date'];
+					$article_body_abbv = substr($row['article_body'],0, 20);
+			
+		?>
+				
+		 		
+		 		<tbody>
+		 			<tr>
+						<td><?php echo "{$article_id}"; ?></td>
+						<td><?php echo "{$article_title}"; ?></td>
+						<td><?php echo "{$article_date}"; ?></td>
+						<td><?php echo "{$article_author}"; ?></td>
+						<td><?php echo "{$article_body_abbv}"; ?></td>
+						<td><a href="#">Edit</a></td>
+						<td><a href="#">Delete</a></td>
+		 			</tr>
+		 			<?php } //Loop through the results ?>
+		 			
+		 		</tbody>
+		 		
+		 	</table>
--- admin/index.php
+++ admin/index.php
@@ -14,58 +14,66 @@
 		
 	<div class="jumbotron">
 		<h1 class="text-center display-2 text-uppercase">Admin Page</h1>
-		<h2 class="text-center display-4 ">View All Posts</h2>
+		
 		
 	</div>
 	 
 	 <section id="admin_area">
+	 
+	 	<!--ADMIN NAV-->
+	 	<div class="card-group">
+		  <div class="card">
+			
+			<div class="card-block text-center">
+			  <i class="fa fa-bullseye fa-5x card-img-top" aria-hidden="true"></i>
+			  <h4 class="card-title ">Goals</h4>
+			  
+			  <a href="" class="btn btn-info">View All Goals</a>
+			  <a href="" class="btn btn-info">Create Goal</a>
+			</div>
+		  </div>
+		  <div class="card">
+			
+			<div class="card-block text-center">
+			  <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+			  <h4 class="card-title">Articles</h4>
+			  <a href="" class="btn btn-info">View All Articles</a>
+			  <a href="" class="btn btn-info">Create Article</a>
+			</div>
+		  </div>
+		  <div class="card">
+			
+			<div class="card-block text-center">
+			  <i class="fa fa-book fa-5x" aria-hidden="true"></i>
+			  <h4 class="card-title">Users</h4>
+			  <a href="" class="btn btn-info">View All Users</a>
+			  <a href="" class="btn btn-info">Create User</a>
+			</div>
+		  </div>
+		</div>
+		
+		
 		 <div id="admin_content">
-		 	<table class="table table-striped table-hover">
-		 		<thead class="thead-inverse">
-		 			<th>Article ID</th>
-		 			<th>Article Title</th>
-		 			<th>Article Author</th>
-		 			<th>Article Date</th>
-		 			<th>Article Body</th>
-		 			<th>Edit</th>
-		 			<th>Delete</th>
-		 		</thead>
-		 		
-		 		<?php
-				//CREATE QUERY
-				$query = "SELECT * FROM articles";
-				
-				//CONNECT QUERY TO DB
-				$show_all_articles= mysqli_query($db,$query);
-				
-				//Loop through and display results
-				while($row =mysqli_fetch_assoc($show_all_articles)){
-			
-					//Creating variables for the array
-					$article_id = $row['article_id'];
-					$article_title = $row['article_title'];
-					$article_author = $row['article_author'];
-					$article_date = $row['article_date'];
-					$article_body_abbv = substr($row['article_body'],0, 20);
-			
-		?>
-				
-		 		
-		 		<tbody>
-		 			<td><?php echo "{$article_id}"; ?></td>
-		 			<td><?php echo "{$article_title}"; ?></td>
-		 			<td><?php echo "{$article_date}"; ?></td>
-		 			<td><?php echo "{$article_author}"; ?></td>
-		 			<td><?php echo "{$article_body_abbv}"; ?></td>
-		 			<td><a href="#">Edit</a></td>
-		 			<td><a href="#">Delete</a></td>
-		 			
-		 			<?php } //Loop through the results ?>
-		 		</tbody>
-		 		
-		 	</table>
+		 	
+		 <!--ADMIN INCLUDES-->
+		 	<!--A dynamic area to display admin content -->
+		 
+		 	<?php 
+			 //CHECKING IF $source IS SET
+			 if(isset($_GET['source'])){
+				$source= $_GET['source'];
+			 }else {
+				 $source="";
+			 }
+			 
+			 switch($source){
+					 
+					 
+			 }
+			 
+			 ?>
 
-		 </div>
+		 </div><!--End of admin_content-->
 	 </section>
 
 	<?php //FOOTER INCLUDES ?>
