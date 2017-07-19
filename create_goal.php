1cb7ce028c1e7975396f772e6605722cc755653e
CQuinz <christian.quinlan36@gmail.com>
2017-07-19 21:12:29 +0100

    added query to insert goals data into the goals table. need to look up syntax to change the date format in SQL table

--- create_goal.php
+++ create_goal.php
@@ -8,8 +8,11 @@
 		<h1 class="font-weight-bold text-center">Create My Goal</h1>
 		<p>Let's create your goal</p>
 		
+		<form action="" method="post">
+		
 		<section id="goal_details">
-			
+		
+				
 			
 			<!--FIRST CARD DECK----------------------------------------------------->
 			
@@ -23,8 +26,8 @@
 				
 				<div class="card-footer">
 				  <div class="form-group">
-					<label for="article_content" class="font-weight-bold">Goal:</label>
-					<input type="text" class="form-control" placeholder="Name of goal">
+					<label for="goal_name" class="font-weight-bold">Goal:</label>
+					<input type="text" class="form-control" placeholder="Name of goal" name="goal_name">
 				  </div>
 				</div>
 			  </div><!--CARD-->
@@ -56,7 +59,7 @@
 				</div>
 				<div class="card-footer">
 			  		<div class="form-group">
-					  <label for="article_content" class="font-weight-bold">Catagory:</label>
+					  <label for="goal_catagory" class="font-weight-bold">Catagory:</label>
 					  <select name="goal_catagory" id="" class="form-control">
 					  	<option value="unspecified">Unspecified</option>
 						<option value="health">Health</option>
@@ -85,8 +88,8 @@
 
 					<div class="card-footer">
 					  <div class="form-group">
-						<label for="article_content" class="font-weight-bold">Specific:</label>
-						<textarea type="text" placeholder="Goal details go here" name="article_body" class="form-control" rows="2"></textarea>
+						<label for="goal_details" class="font-weight-bold">How will you know:</label>
+						<textarea type="text" placeholder="Success looks like" name="goal_details" class="form-control" rows="2"></textarea>
 					  </div>
 					</div>
 				  </div><!--CARD-->
@@ -111,13 +114,38 @@
 			
 			<div id="goal_footer" class="text-center my-5">
 				<p>Now let's add this goal and move on the the next step of creating your <a href="">Action Plan!</a></p>
-				<input type="submit" class="btn btn-primary" value="Add Goal">
-				
+				<input type="submit" class="btn btn-primary" value="Add Goal" name="add_goal">
 			</div>
 			
+		<?php 
+	if($db){
+		echo "we are connected";
 		
+	}else{
+		die("no connection". mysqli_error($db));
+	}
+	
+			if(isset($_POST['add_goal'])){
+				
+				$goal_title = $_POST['goal_name'];
+				$goal_details = $_POST['goal_details'];
+				$goal_comp_date = $_POST['goal_comp_date'];
+				$goal_type = $_POST['goal_type'];
+				//$goal_status = $_POST['goal_status'];
+				$goal_catagory = $_POST['goal_catagory'];
+				
+				/*ADD CONDITIONAL TO CHECK IF GOAL TYPE IS SET TO ONGOING OR DEADLINE*/
+				
+				$query = "INSERT INTO goals (goal_title, goal_details, goal_finish, goal_type, goal_status, goal_catagory) ";
+				$query .="VALUES ('{$goal_title}', '{$goal_details}',{$goal_comp_date},'{$goal_type}','Active','{$goal_catagory}')";
+				
+				$add_goal_query =mysqli_query($db,$query);
+		}
+			
+		?>
 			
 		</section>
+		</form>
 			
 		
 		
