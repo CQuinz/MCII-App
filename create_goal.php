626b0bf105b4af34f9b2f2b03f2c03c0e2a8fe88
CQuinz <christian.quinlan36@gmail.com>
2017-07-19 20:04:45 +0100

    improved create_goals layout. needs more work but will depend on time

--- create_goal.php
+++ create_goal.php
@@ -9,21 +9,13 @@
 		<p>Let's create your goal</p>
 		
 		<section id="goal_details">
-			<div class="form-group justify-content-center">
-				<label for="goal_name" class="font-weight-bold">The name of your goal:</label>
-				<input type="text" name="goal_name" class="form-control">
-			</div>
-			<div class="form-group justify-content-center">
-				<label for="goal_name" class="font-weight-bold">Goal Type:</label>
-				<input type="text" name="goal_name" class="form-control">
-				<label for="goal_name" class="font-weight-bold">Goal Status:</label>
-				<input type="text" name="goal_name" class="form-control">
-			</div>
+			
+			
+			<!--FIRST CARD DECK----------------------------------------------------->
 			
 			<div class="card-deck">
+		  
 			  <div class="card">
-<!--				<img class="card-img-top" src="..." alt="Card image cap">-->
-					
 				<div class="card-block">
 				  <h4 class="card-title text-center">Goal Name</h4>
 				  <p class="card-text">Let's create a name for this goal. What do you want to achieve?</p>
@@ -32,8 +24,25 @@
 				<div class="card-footer">
 				  <div class="form-group">
 					<label for="article_content" class="font-weight-bold">Goal:</label>
-					<input type="text" class="form-control">
+					<input type="text" class="form-control" placeholder="Name of goal">
 				  </div>
+				</div>
+			  </div><!--CARD-->
+			  
+			    <div class="card">
+
+				<div class="card-block">
+				  <h4 class="card-title text-center">Goal Type</h4>
+					<p class="card-text">Will this goal have a deadline or will it be ongoing?</p>
+				</div>
+				<div class="card-footer">
+			  		<div class="form-group">
+					  <label for="goal_type" class="font-weight-bold">Type:</label>
+					  <select name="goal_type" id="" class="form-control">
+					  	<option value="ongoing">Ongoing</option>
+						<option value="deadline">Deadline</option>
+					  </select>
+					 </div>
 				</div>
 			  </div><!--CARD-->
 			  
@@ -60,43 +69,47 @@
 				</div>
 			  </div><!--CARD-->
 			  
-			  <div class="card">
-<!--				<img class="card-img-top" src="..." alt="Card image cap">-->
-				
-				<div class="card-block">
-				  <h4 class="card-title text-center">Get Specific</h4>
-				  <p class="card-text">We're going to need to get a little bit more detailed. In measurable terms - how will you know when you've reached your goal? What would you (or someone else) see, hear or feel?</p>  
-				</div>
-				
-				
-				<div class="card-footer">
-				  <div class="form-group">
-					<label for="article_content" class="font-weight-bold">Specific:</label>
-					<textarea type="text" placeholder="Goal details go here" name="article_body" class="form-control" rows="2"></textarea>
-				  </div>
-				</div>
-			  </div><!--CARD-->
 			  
-			   <div class="card">
-<!--				<img class="card-img-top" src="..." alt="Card image cap">-->
-				
-				<div class="card-block">
-				  <h4 class="card-title text-center">Set a Date</h4>
-				  <p class="card-text">Ideally when would you like to have this goal completed? If the goal is ongoing, feel free to skip this question</p>  
-				</div>
-				
-				
-				<div class="card-footer">
-				  <div class="form-group">
-					<label for="article_content" class="font-weight-bold">Completed by:</label>
-					<input type="date"  name="goal_comp_date" class="form-control">
-				  </div>
-				</div>
-			  </div><!--CARD-->
-			  
-			</div><!--CARD DECK-->
+			</div><!--END OF FIRST CARD DECK---------------------------------------------------------->
 			
-			<div id="goal_footer" class="text-center">
+			
+			<!--SECOND CARD DECK----------------------------------------------------------------------->
+			
+			<div class="card-deck">
+			
+				<div class="card">
+					<div class="card-block">
+					  <h4 class="card-title text-center">Get Specific</h4>
+					  <p class="card-text">We're going to need to get a little bit more detailed. In measurable terms - how will you know when you've reached your goal? What would you (or someone else) see, hear or feel?</p>  
+					</div>
+
+					<div class="card-footer">
+					  <div class="form-group">
+						<label for="article_content" class="font-weight-bold">Specific:</label>
+						<textarea type="text" placeholder="Goal details go here" name="article_body" class="form-control" rows="2"></textarea>
+					  </div>
+					</div>
+				  </div><!--CARD-->
+
+				   <div class="card">
+					<div class="card-block">
+					  <h4 class="card-title text-center">Set a Date</h4>
+					  <p class="card-text">Ideally when would you like to have this goal completed? If the goal is ongoing, feel free to skip this question</p>  
+					</div>
+
+					<div class="card-footer">
+					  <div class="form-group">
+						<label for="article_content" class="font-weight-bold">Completed by:</label>
+						<input type="date"  name="goal_comp_date" class="form-control">
+					  </div>
+					</div>
+				  </div><!--CARD-->
+	
+			  
+			</div><!--END OF SECOND CARD DECK------------------------------------------------------------------------->
+			
+			
+			<div id="goal_footer" class="text-center my-5">
 				<p>Now let's add this goal and move on the the next step of creating your <a href="">Action Plan!</a></p>
 				<input type="submit" class="btn btn-primary" value="Add Goal">
 				
