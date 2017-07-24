7d7df931bb1a71e1ac6bef7848c9bff75dcf1470
CQuinz <christian.quinlan36@gmail.com>
2017-07-24 18:32:56 +0100

    created dynamic tables for my goals page. displays all the goals and tier data. need to create delete and edit quieries PLUS need to make db relational

--- my_goals.php
+++ my_goals.php
@@ -23,86 +23,119 @@
       <!--ARTICLES-->
       	<div class="col-md-10 offset-md-1">
       	
-      	<h3 class="text-center">My Goals</h3>
-			
-	 		<table class="table table-striped table-bordered table-hover">
-		 		<thead class="thead-inverse">
-		 			<tr>
-						
-						<th>Goal Title</th>
-						<th>Goal Status</th>
-						<th>Goal Catagory</th>
-						<th>Goal Start</th>
-						<th>Goal Finish</th>
-						<th>Edit</th>
-						<th>Delete</th>
-					</tr>
-		 		</thead>
-		 		
-		 		<?php
-				//CREATE QUERY SHOW ALL ARTICLES
-				$query = "SELECT * FROM goals";
-				
-				//CONNECT QUERY TO DB
-				$show_all_goals= mysqli_query($db,$query);
-				
-				//Loop through and display results
-				while($row =mysqli_fetch_assoc($show_all_goals)){
-			
-					//Creating variables for the array
-					$goal_id = $row['goal_id'];
-					//$goal_id = $row['goal_id'];
-					$goal_title = $row['goal_title'];
-					$goal_user_id = $row['goal_user_id'];
-					$goal_start_date = $row['goal_start_date'];
-					$goal_comp_date = $row['goal_comp_date'];
-					$goal_status = $row['goal_status'];
-					$goal_catagory = $row['goal_catagory'];
+      
+      	
+      	<!--QUERY ALL GOALS FOR THIS USER AND DISPLAY DATA IN A SET OF TABLES (OUTTER TABLE IS GOALS - INNER IS GOAL_TIERS) -->
+      	
+      	<?php
+		//CREATE QUERY SHOW ALL ARTICLES
+		$query = "SELECT * FROM goals";
+
+		//CONNECT QUERY TO DB
+		$show_all_goals= mysqli_query($db,$query);
+
+		//Loop through and display results
+		while($row =mysqli_fetch_assoc($show_all_goals)){
+
+			//Creating variables for the array
+			$goal_id = $row['goal_id'];
+			//$goal_id = $row['goal_id'];
+			$goal_title = $row['goal_title'];
+			$goal_user_id = $row['goal_user_id'];
+			$goal_start_date = $row['goal_start_date'];
+			$goal_comp_date = $row['goal_comp_date'];
+			$goal_status = $row['goal_status'];
+			$goal_catagory = $row['goal_catagory'];
 			
 		?>
-				
-		 		
-		 		<tbody>
-		 			<tr>
-						<td><?php echo "{$goal_title}"; ?></td>
-						<td><?php echo "{$goal_status}"; ?></td>	
-						<td><?php echo "{$goal_catagory}"; ?></td>
-						<td><?php echo "{$goal_start_date}"; ?></td>
-						<td><?php echo "{$goal_comp_date}"; ?></td>
-						<td><?php //echo "<a href='index.php?source=edit_article&edit={$article_id}'>Edit</a>"; ?></td>
-						<td><?php //echo "<a href='index.php?source=view_articles&delete={$article_id}'>Delete</a>"; ?></td>
-		 			</tr>
-		 		<thead class="thead-default">
-		 			<tr>
-		 				<th>Benifit</th>
-		 				<th>Obstacle</th>
-		 				<th>Plan</th>
-		 			</tr>
-		 		</thead>
-		 		
-		 				
-		 								
-		 			<?php } //Loop through the results ?>
-		 			
-		 			<?php 
-					//DELETE ARTICLES QUERY
-//					if(isset($_GET['delete'])){
-//						
-//						$delete_article_key = $_GET['delete'];
-//						$query ="DELETE FROM articles WHERE article_id= {$delete_article_key}";
-//						$delete_article_query = mysqli_query($db,$query);
-//						
-//						header("Location: index.php?source=view_articles");
-//						
-//					}
+      	
+			<!--OUTTER TABLE FOR GOALS-->
+			<div class="card">
+				<div class="w-100 p-2 bg-primary text-white">
+					<h3 class="text-center"><?php echo "{$goal_title}"; ?></h3>
+				</div>
+				<table class="table table-striped table-bordered table-hover text-center">
+					<thead class="bg-primary text-white">
+						<tr>
+							<th>Goal Title</th>
+							<th>Goal Status</th>
+							<th>Goal Catagory</th>
+							<th>Goal Start</th>
+							<th>Goal Finish</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
 					
+					<!--DELETE GOAL QUERY-->
+					<?php 
+					if(isset($_GET['delete_goal_id'])){
+						$query= "DELETE FROM goals WHERE goal_id = $goal_id";
+						$deleteGoalQuery= mysqli_query($db,$query);
+						header("Location: my_goals.php");
+						}
 					?>
+
+					<tbody>
+						<tr>
+							<td><?php echo "{$goal_title}"; ?></td>
+							<td><?php echo "{$goal_status}"; ?></td>	
+							<td><?php echo "{$goal_catagory}"; ?></td>
+							<td><?php echo "{$goal_start_date}"; ?></td>
+							<td><?php echo "{$goal_comp_date}"; ?></td>
+							<td><?php echo "<a href='index.php?source=edit_article&edit=$goal_id'>Edit</a>"; ?></td>
+							<td><?php echo "<a href='index.php?delete_goal_id=$goal_id'>Delete</a>"; ?></td>
+						</tr>
+					</tbody>
+
+					<!--INNER TABLE FOR GOAL_TIERS-->
+						<?php
+						//CREATE QUERY SHOW ALL ARTICLES
+						$queryInner = "SELECT * FROM goal_tier WHERE goal_id = $goal_id";
+
+						//CONNECT QUERY TO DB
+						$show_users_tiers= mysqli_query($db,$queryInner);
+
+						//Loop through and display results
+						while($row =mysqli_fetch_assoc($show_users_tiers)){
+
+							//Creating variables for the array
+							$tier_id = $row['tier_id'];
+							//$goal_id = $row['goal_id'];
+							$benefit = $row['benefit'];
+							$obstacle = $row['obstacle'];
+							$plan = $row['plan'];
+
+						?>
+
+
+					<table class="table table-striped">
+						<thead class="bg-info text-white">
+							<tr class="text-center">
+								<th>Benefit</th>
+								<th>Obstacle</th>
+								<th>Plan</th>
+								<th>Edit</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><?php echo "{$benefit}"; ?></td>
+								<td><?php echo "{$obstacle}"; ?></td>	
+								<td><?php echo "{$plan}"; ?></td>
+							</tr>
+						</tbody>
+					</table>
+					<?php } ?><!--CLOSE GOAL_TIER WHILE INNER LOOP -->
+				</table>
+			</div>
+			<br>
+		 	<?php } ?><!--CLOSE GOAL WHILE OUTTER LOOP -->
 		 			
-		 			
-		 			
-		 		</tbody>
 		 		
-		 	</table>
+		 		
+		 	
      		
      		<!-- Button trigger modal -->
 			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
